3b7a97117fcb42c4d73ca2181110c214a8b52cb9
4594400 <4594400@gmail.com>
2016-03-20 21:04:54 +0200

    Added module 6 add file from 3 module

---
+++ src/com/goit/module_6/exampleMyException/AudioFile.java
@@ -0,0 +1,6 @@
+package com.goit.module_6.exampleMyException;
+
+
+public class AudioFile extends File {
+
+}
---
+++ src/com/goit/module_6/exampleMyException/TextFile.java
@@ -0,0 +1,6 @@
+package com.goit.module_6.exampleMyException;
+
+
+public class TextFile extends File {
+
+}
